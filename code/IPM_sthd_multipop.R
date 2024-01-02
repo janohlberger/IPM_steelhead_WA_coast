a51cb289e3883a109698257397cd2dcb87c33b77
Jan Ohlberger <janohl@uw.edu>
2024-01-02 14:36:48 -0800

    all fitting options included

--- code/IPM_sthd_multipop.R
+++ code/IPM_sthd_multipop.R
@@ -1,18 +1,14 @@
 ##=================================================================##
 ##                                                                 ##
-##    Fit hierarchical IPM for WA coastal steelhead populations    ##
+##                 Fit integrated population model                 ##
 ##                                                                 ##
 ##=================================================================##
 
 ##========================================================## packages
-pkg<-c("here","dplyr","tidyverse","rstan","readr","readxl","tibble", "dataRetrieval","posterior","ggsidekick","RColorBrewer","ggplot2", "officer","MuMIn","ncdf4","reshape2","pracma","relaimpo","visreg", "Hmisc","bayesdfa","MARSS","faraway","gtools","gridExtra","gsl","rcartocolor","bayesplot","rstanarm","distributional","salmonIPM")
+pkg<-c("here","tidyverse","rstan","gtools","salmonIPM")
 if(length(setdiff(pkg,rownames(installed.packages())))>0){install.packages(setdiff(pkg,rownames(installed.packages())),dependencies=T)}
 invisible(lapply(pkg,library,character.only=T))
 home<-here::here()
-
-##========================================================## settings
-theme_set(theme_sleek())
-options(rstudio.help.showDataPreview=FALSE)
 
 ##===================================================## rstan options
 options(mc.cores=parallel::detectCores())
@@ -23,30 +19,30 @@ out_dir<-paste0(home,"/output/")
 if(!file.exists(out_dir)) dir.create(file.path(out_dir))
 setwd(file.path(out_dir))
 
-##=======================================================## read data
-fish_dat<-read.csv("IPM_fish_dat_all.csv")
-covar_dat<-read.csv("IPM_covar_dat_all.csv")
-
-##=========================================================## fit IPM
+##========================================================## settings
 covar_effects<-TRUE ## TRUE or FALSE
 year_effect<-FALSE ## TRUE or FALSE
 SR_mod<-"Ricker" ## SR function (Ricker/BH)
-##----------------------------------------------------------## priors
-priors<- list(
-   mu_alpha ~ normal(1.5,0.5), ## hyper mean/SD of log productivity
-   mu_p ~ dirichlet(c(1,2,47,44,5,1)), ## mean maiden spawner age dist
-   mu_SS ~ beta(1.5,3) ## mean kelt survival rate across years
-)
-##----------------------------------------------## add covariate data
-if(covar_effects) { ## no NAs in covariates
+
+##============================================================## data
+fish_dat<-read.csv(paste0(home,"/data/IPM_fish_dat_all.csv"))
+covar_dat<-read.csv(paste0(home,"/data/IPM_covar_dat_all.csv"))
+
+## no NAs allowed in covariate data
+if(covar_effects) { 
    fish_dat<-fish_dat %>%
       left_join(covar_dat,by='year') %>%
-      na.omit()
-   par_models<-list(s_SS~NPGO+SST+pinks,
-                    R~NPGO_2+SST_4+pinks_4+av_CFS_min_1)
+      na.omit() 
+}
+
+dat_years<-sort(unique(fish_dat$year)) ## years included
+nY<-length(dat_years) ## number of years
+
+##======================================## regressions to be included
+if(covar_effects) {
+   par_models<-list(s_SS~NPGO+SST+pinks,R~NPGO_2+SST_4+pinks_4+av_CFS_min_1)
    if(year_effect) { 
-      par_models<-list(s_SS~year+NPGO+SST+pinks,
-                       R~year+NPGO_2+SST_4+pinks_4+av_CFS_min_1)
+      par_models<-list(s_SS~year+NPGO+SST+pinks,R~year+NPGO_2+SST_4+pinks_4+av_CFS_min_1)
    }
 } else {
    if(year_effect) { 
@@ -55,58 +51,45 @@ if(covar_effects) { ## no NAs in covariates
       par_models<-NULL
    }
 }
-##--------------------------------------------------## years included
-dat_years<-sort(unique(fish_dat$year))
-nY<-length(dat_years)
-##-----------------------------------------## save data used in model
-if(covar_effects) { 
-   if(year_effect) { 
-      write.csv(fish_dat,"IPM_fish_dat_with_covars_with_year.csv",
-                row.names=F)
-   }else{
-      write.csv(fish_dat,"IPM_fish_dat_with_covars.csv",
-                row.names=F)
-   }
-}else{ 
-   if(year_effect) { 
-      write.csv(fish_dat,"IPM_fish_dat_without_covars_with_year.csv",
-                row.names=F)
-   }else{
-      write.csv(fish_dat,"IPM_fish_dat_without_covars.csv",
-                row.names=F)
-   }
-}
-##-------------------------------------------------------## fit model
+
+##=========================================================## fit IPM
 IPM_fit<-salmonIPM(
    life_cycle="SSiter",
    pool_pops=TRUE,
    SR_fun=SR_mod, 
    par_models=par_models, 
    fish_data=fish_dat,
-   prior=priors,
+   prior=list(
+      mu_alpha~normal(1.5,0.5),
+      mu_p~dirichlet(c(1,2,47,44,5,1)),
+      mu_SS~beta(1.5,3)),
    pars=c(stan_pars("IPM_SSiter_pp")),
    chains=3, 
    iter=2000,
    warmup=1000, 
    control=list(adapt_delta=0.95,max_treedepth=10)
-) ## adapt_delta/max_treedepth: tests=0.95/10 | final=0.99/12
-##----------------------------------------------------## time elapsed
+   # control=list(adapt_delta=0.99,max_treedepth=12)
+)
+
 print(paste(round(max(rowSums(get_elapsed_time(IPM_fit))/60)),"min"))
-##----------------------------------------------------## save results
+
+##=====================================## save model data and results
 if(covar_effects) { 
    if(year_effect) { 
+      write.csv(fish_dat,"IPM_fish_dat_with_covars_with_year.csv",row.names=F)
       saveRDS(IPM_fit,"IPM_fit_with_covars_with_year.Rdata")
    }else{
+      write.csv(fish_dat,"IPM_fish_dat_with_covars.csv",row.names=F)
       saveRDS(IPM_fit,"IPM_fit_with_covars.Rdata")
    }
 }else{ 
    if(year_effect) { 
+      write.csv(fish_dat,"IPM_fish_dat_without_covars_with_year.csv",row.names=F)
       saveRDS(IPM_fit,"IPM_fit_without_covars_with_year.Rdata") 
    }else{
+      write.csv(fish_dat,"IPM_fish_dat_without_covars.csv",row.names=F)
       saveRDS(IPM_fit,"IPM_fit_without_covars.Rdata") 
    }
 }
 
-##=================================================================##
-##=================================================================##
 ##=================================================================##
